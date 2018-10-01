c36a87a6cecf743cca1619af95e2ed320abe1153
Roby <robyeka@email.unikom.ac.id>
2018-10-01 22:37:37 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan18/kelipatan/PBO6K10116599Latihan18Kelipatan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program kelipatan yang menampilkan
+ *                      bilangan kelipatan 3,5 dimulai dari 3,5 sampai dengan 35.
+ * 
+ */
+public class PBO6K10116599Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        // for (double i = 3.5; i<=35; i++) {
+        // System.out.println(i);
+    //}
+    
+    for(double x=3; x<36; x++)
+        if (x%3==0)
+        System.out.println(" "+x);
+    
+    }
+    
+}
